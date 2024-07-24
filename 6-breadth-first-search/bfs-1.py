b64175eeec58886df92bab0996033d9b7c41416a
david <davidmovsichoff@gmail.com>
2024-07-23 22:53:39 -0300

    bfs shortest path in progress

--- 6-breadth-first-search/bfs-1.py
+++ 6-breadth-first-search/bfs-1.py
@@ -10,20 +10,25 @@ network = {
 }
 
 def bfs(graph, start, target):
-    searchlist = graph[start].copy()
-    visited = []
-    idx = 0
-    while idx < len(searchlist):
-        current = searchlist[idx]
-        if current not in visited:
-            print(current)
-            if current == target:
-                print('found')
-                return 1
-            visited.append(current)
-            searchlist.extend(graph[current])
-        idx += 1
+    
+    searchlist = list(map(lambda node: (node, 1), graph[start]))
+    visited = [(start, 0)]
 
-bfs(network, 'you', 'thom')
+    while searchlist:
+        current = searchlist[0]
+        if current[0] == target:
+            print(visited)
+            return True
+        visited.append(current)
+
+        for neighbour in graph[current[0]]:
+            if neighbour not in visited: ## TODO: Make this condition compare only the value of the node
+                searchlist.append((neighbour, current[1]+1))
+        
+        searchlist.pop(0)
+
+print( bfs(network, 'you', 'anuj') )
 # This answer the question 1/2 of bfs: "Is there a path from A to B?"
-# Now, how can I refactor it to also answer "Which is the shortest way from A to B?"
+# In this case, "Is there a path from you to Anuj through your network of friends?"
+
+# TODO: Think about a way to get the shortest path from A to B.
