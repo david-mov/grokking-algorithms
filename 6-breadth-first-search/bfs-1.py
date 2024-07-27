0a8b9811154f4bdcc7a0dc03de080f4d7940006b
David <davidmovsichoff@gmail.com>
2024-07-27 17:48:24 -0300

    find a path from A to B with bfs algo

--- 6-breadth-first-search/bfs-1.py
+++ 6-breadth-first-search/bfs-1.py
@@ -10,25 +10,23 @@ network = {
 }
 
 def bfs(graph, start, target):
-    
-    searchlist = list(map(lambda node: (node, 1), graph[start]))
-    visited = [(start, 0)]
+    searchlist = graph[start].copy()
+    visited = [start]
 
     while searchlist:
         current = searchlist[0]
-        if current[0] == target:
+        if current == target:
             print(visited)
             return True
+        
         visited.append(current)
 
-        for neighbour in graph[current[0]]:
-            if neighbour not in visited: ## TODO: Make this condition compare only the value of the node
-                searchlist.append((neighbour, current[1]+1))
+        for neighbour in graph[current]:
+            if neighbour not in visited:
+                searchlist.append(neighbour)
         
         searchlist.pop(0)
 
 print( bfs(network, 'you', 'anuj') )
-# This answer the question 1/2 of bfs: "Is there a path from A to B?"
+# This algorithm shows how to solve the problem: "Is there a path from A to B?"
 # In this case, "Is there a path from you to Anuj through your network of friends?"
-
-# TODO: Think about a way to get the shortest path from A to B.
