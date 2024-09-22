a34779a3b2268da754c1d79c96ad187c4cacca1d
David <davidmovsichoff@gmail.com>
2024-09-22 20:33:40 -0300

    dijkstra finished.py

--- 7-dijkstra/dijkstra.py
+++ 7-dijkstra/dijkstra.py
@@ -1,4 +1,4 @@
-trades = {
+trtrades = {
     "book": {"lp": 5, "poster": 0},
     "lp": {"guitar": 15, "drum": 20},
     "poster": {"guitar": 30, "drum": 35},
@@ -9,19 +9,38 @@ trades = {
 
 infinity = float('inf')
 
+def cheapest_node(costs, processed):
+        cheapest = None
+        cheapest_cost = infinity
+        for neighbour, cost in costs.items():
+            if neighbour not in processed:
+                if cost < cheapest_cost:
+                    cheapest = neighbour
+                    cheapest_cost = cost
+        return cheapest
+
+def create_path(graph, start, target):
+    current = start
+    path = []
+    while current:
+        path.insert(0, current)
+        current = graph.get(current)
+    return path
+
 def dijkstra(graph, start, target):
     nodes = graph.keys()
     costs = dict.fromkeys(nodes, infinity)
     costs[start] = 0
     parents = {}
     processed = [start]
-
-    current = start
     
+    current = start
     while len(processed) < len(nodes):
+        if current == target:
+            return (costs[target], create_path(parents, target, start))
+        
         processed.append(current)
-        cheapest = None
-        cheapest_cost = infinity
+        
         for neighbour, cost in graph[current].items():
             if neighbour not in processed:
                 
@@ -29,12 +48,9 @@ def dijkstra(graph, start, target):
                 if new_cost < costs[neighbour]:
                     costs[neighbour] = new_cost
                     parents[neighbour] = current
-            
-            if cost < cheapest_cost:
-                cheapest = neighbour
-                cheapest_cost = cost
-                
-        current = cheapest # make a pending list to process neighbours from cheapest to most expensive
-    return costs
+        
+        current = cheapest_node(costs, processed)
+        
+    return (costs[target], create_path(parents, target, start))
         
 print( dijkstra(trades, "book", "piano") )
